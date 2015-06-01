1e8a9dc9e782cd31e5c428990aea80a18b115d49
Dana Peele <dana_peele@s5a.com>
2015-06-01 09:55:45 -0400

    Changes to vagrant and puppet

--- puppet/site.pp
+++ puppet/site.pp
@@ -1,23 +1,49 @@
 include 'sbtubuntu'
 
-include class { 'java':
-    package => 'openjdk-8-jdk'
+class { 'java':
+  package => 'openjdk-7-jdk'
 }
 
 class { 'nodejs':
-    version => 'latest',
+  version => 'latest'
 }
 
-class {'::mongodb::server':
-  auth => true,
-}
+include '::mongodb::server'
 
-mongodb::db {'shared_state_inventory':
-  user          => 'shared_state_inventory',
-  password_hash => 'shared_state_inventory_123',
-}
+include '::apache'
 
-class {'graphite':}
+apache::vhost { 'graphite.inventory.com':
+  port    => '80',
+  docroot => '/opt/graphite/webapp',
+  wsgi_application_group      => '%{GLOBAL}',
+  wsgi_daemon_process         => 'graphite',
+  wsgi_daemon_process_options => {
+    processes          => '5',
+    threads            => '5',
+    display-name       => '%{GROUP}',
+    inactivity-timeout => '120',
+  },
+  wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
+  wsgi_import_script_options  => {
+    process-group     => 'graphite',
+    application-group => '%{GLOBAL}'
+  },
+  wsgi_process_group          => 'graphite',
+  wsgi_script_aliases         => {
+    '/' => '/opt/graphite/conf/graphite.wsgi'
+  },
+  headers => [
+    'set Access-Control-Allow-Origin "*"',
+    'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
+    'set Access-Control-Allow-Headers "origin, authorization, accept"',
+  ],
+  directories => [{
+    path => '/media/'}
+  ]
+}->
+class {'graphite':
+  gr_web_server => 'none'
+}
 
 class {'statsd':
   backends     => ['./backends/graphite'],
