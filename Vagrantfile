3c63815fce6b7c77010c6cbc71916245d626fdbd
Dana Peele <dana_peele@s5a.com>
2015-06-01 17:22:50 -0400

    Vagrant and puppet work to fix graphite
    Conflicts:
    	puppet/site.pp

--- Vagrantfile
+++ Vagrantfile
@@ -3,7 +3,7 @@
 
 Vagrant.configure(2) do |config|
   config.vm.box = "ubuntu/trusty64"
-  config.vm.network "private_network", ip: "196.168.44.22"
+  config.vm.network "private_network", ip: "192.168.44.22"
   config.vm.provider "virtualbox" do |vb|
     vb.memory = "4096"
   end
--- puppet/site.pp
+++ puppet/site.pp
@@ -1,52 +1,11 @@
 include 'sbtubuntu'
+include 'docker'
 
 class { 'java':
   package => 'openjdk-7-jdk'
 }
 
-class { 'nodejs':
-  version => 'latest'
-}
-
-include '::mongodb::server'
-
-include '::apache'
-
-apache::vhost { 'graphite.inventory.com':
-  port    => '80',
-  docroot => '/opt/graphite/webapp',
-  wsgi_application_group      => '%{GLOBAL}',
-  wsgi_daemon_process         => 'graphite',
-  wsgi_daemon_process_options => {
-    processes          => '5',
-    threads            => '5',
-    display-name       => '%{GROUP}',
-    inactivity-timeout => '120',
-  },
-  wsgi_import_script          => '/opt/graphite/conf/graphite.wsgi',
-  wsgi_import_script_options  => {
-    process-group     => 'graphite',
-    application-group => '%{GLOBAL}'
-  },
-  wsgi_process_group          => 'graphite',
-  wsgi_script_aliases         => {
-    '/' => '/opt/graphite/conf/graphite.wsgi'
-  },
-  headers => [
-    'set Access-Control-Allow-Origin "*"',
-    'set Access-Control-Allow-Methods "GET, OPTIONS, POST"',
-    'set Access-Control-Allow-Headers "origin, authorization, accept"',
-  ],
-  directories => [{
-    path => '/media/'}
-  ]
-}->
-class {'graphite':
-  gr_web_server => 'none'
-}
-
-class {'statsd':
-  backends     => ['./backends/graphite'],
-  graphiteHost => 'localhost',
-  require      => Class['nodejs']
+docker::run { 'graphite':
+  image  => 'hopsoft/graphite-statsd',
+  ports => ["80:80", "2003:2003", "8125:8125/udp"]
 }
